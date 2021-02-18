1d191a8ea75fea373db1c3196febe8862f4ef5f0
Pablo de Tuero <pablo91th@gmail.com>
2021-02-18 23:00:56 +0100

    Agregar archivos de proyecto.

---
+++ Figuras/FIGURAS.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Figuras
+{
+    class FIGURAS
+    {
+        double area = 0;
+        double lado = 0;
+        double ancho = 0;
+        double radio = 0;
+
+        public void areaCuadrado()
+        {
+            Console.WriteLine("Indica el lado: ");
+            lado = Convert.ToDouble(Console.ReadLine());
+            area = lado * lado;
+            Console.WriteLine("El resultado del area es: " + area);
+            Console.WriteLine(". . .");
+            Console.WriteLine(".   .");
+            Console.WriteLine(". . .");
+        }
+        public void areaRectangulo()
+        {
+            Console.WriteLine("Indica el largo: ");
+            lado = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Indica el ancho: ");
+            lado = Convert.ToDouble(Console.ReadLine());
+            area = lado * ancho;
+            Console.WriteLine("El resultado del area es: " + area);
+            Console.WriteLine(". . . .");
+            Console.WriteLine(".     .");
+            Console.WriteLine(". . . .");
+        }
+        public void areaCirculo()
+        {
+            Console.WriteLine("Indica el radio: ");
+            radio = Convert.ToDouble(Console.ReadLine());
+            area = Math.PI*Math.Pow(radio,2);
+            Console.WriteLine("El resultado del area es: " + area);
+            Console.WriteLine(" .  .");
+            Console.WriteLine(".    .");
+            Console.WriteLine(".    .");
+            Console.WriteLine(" .  .");
+        }
+    }
+}
---
+++ Figuras/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Figuras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int n = 1;
+                for (int i = 0; i < n; i++)
+                {
+                    Console.WriteLine("¿Desea utilizar la aplicación para el cálculo del área de alguna figura? <S/N>");
+                    string valor = Console.ReadLine();
+
+                    if (valor.ToUpper() == "S")
+                    {
+                        Console.WriteLine("Elige la figura del área que desea calcular: \n" +
+                                        "1. AREA DE UN CUADRADO \n" + "2. AREA DE UN RECTANGULO \n" + "3. AREA DE UN CIRCULO");
+
+                        string opcion = Console.ReadLine();
+
+                        FIGURAS fIGURAS = new FIGURAS();
+
+                        switch (opcion)
+                        {
+                            case "1":
+                                fIGURAS.areaCuadrado();
+                                break;
+                            case "2":
+                                fIGURAS.areaRectangulo();
+                                break;
+                            case "3":
+                                fIGURAS.areaCirculo();
+                                break;
+                            default:
+                                Console.WriteLine("OPCION ELEGIDA NO VALIDA");
+                                break;
+                        }
+                        n++;
+                    }
+                    else if (valor == "N" || valor == "n")
+                    {
+                        return;
+                    }
+                }
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("La aplicación ha fallado. Contacte con soporte. " + ex.Message);
+            }
+            
+        }
+    }
+}
